c019aa4adcdf4cfa1063b75e99134e0d459796f1
MightBeASlut <38793289+MightBeASlut@users.noreply.github.com>
2018-06-09 13:45:03 -0700

    Update suggestions.js

--- commands/suggestions.js
+++ commands/suggestions.js
@@ -1,20 +1,10 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args, member, guild, size) => {
-    let channel = message.channel;
+module.exports.run = async (bot, message, args) => {
 
-    var msg = message.content;
-    var args = msg.split(" ")
-    let suggestion = args.join(" ").slice(9);
-   if(!message.channel.id === "channel id ") return message.channel.send("Wrong channel"); 
-   
-if (!args[0]) {
-        message.delete()
-        message.author.send("Correct usage: -suggest *suggestion here*");
-    }
-    if (args[1]) {
-        message.delete()
-    let arg = message.content.split(" ").slice(10);
+   let suggestion = args.join(" ");
+   if(message.channel.id !== "449924263659438090") return message.channel.send("Wrong channel");
+  if(!args[0] || args[0] === "help") return message.channel.send(" -suggestion <suggestion>");
 
     let suggestionEmbed = new Discord.RichEmbed()
     .setDescription(`Nieuwe suggestie van: ${message.author}`)
@@ -29,17 +19,17 @@ if (!args[0]) {
 		await message.react("❌")
     }).catch(function() {})
 
-    let logs = message.guild.channels.find(`name`, "logs");
+    let logs = message.guild.channels.find("name", "logs");
     let embed = new Discord.RichEmbed()
     .setColor("#ffffff")
     .addField('Discord Console', `${message.author} heeft een nieuwe suggestie gemaakt!`)
     .setFooter(`© ForestMC`, "https://imgur.com/tfBmDbI.png")
-    .setTimestamp()
+    .setTimestamp();
 
     logs.sendEmbed(embed);
+
 };
-}
-}
+
 
 module.exports.help = {
   name: "suggest"
