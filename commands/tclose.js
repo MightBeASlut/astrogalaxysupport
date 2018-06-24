1328ce4155bbeadf78e9ae62783410fb29376cf1
MightBeASlut <38793289+MightBeASlut@users.noreply.github.com>
2018-06-24 05:32:59 -0700

    Update tclose.js

--- commands/tclose.js
+++ commands/tclose.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
   let cName = channel.name;
 
     let channelName = message.channel.name
-    if(cName.startsWith("appeal-") || cName.startsWith("apply-") || cName.startsWith("report-") || cName.startsWith("support-") || cName.startsWith("bug-")) {
+    if(cName.startsWith("appeal-") || cName.startsWith("apply-") || cName.startsWith("report-") || cName.startsWith("help-") || cName.startsWith("bug-")) {
       channel.delete();
     } else {
       return ("Are you sure its a appeal/appeal/report channel?")
